ba89e9f65dbb4f1a26b01cec695e68f72e4bead0
Brahim Lamri <brahim.llmr@gmail.com>
2023-06-09 20:21:50 +0100

    Setup project

---
+++ GameOfLife/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  GameOfLife
+//
+//  Created by Brahim Lamri on 9/6/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GameOfLife/GameOfLifeApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameOfLifeApp.swift
+//  GameOfLife
+//
+//  Created by Brahim Lamri on 9/6/2023.
+//
+
+import SwiftUI
+
+@main
+struct GameOfLifeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
